6aa2185514886b2dda0324731f6bcdefa5359e78
pminorsbda <38075521+pminorsbda@users.noreply.github.com>
2018-04-06 15:54:08 -0300

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
     link = models.URLField(blank=True)
+    photo = models.URLField(blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
